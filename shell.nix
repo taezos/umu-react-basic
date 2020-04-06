68badb3ee5c8c6aebb121fe41f865a33aeaa9612
ken <piq9117@gmail.com>
2020-04-05 21:40:27 -0500

    initial setup

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, ansi-terminal, base, bytestring
+      , classy-prelude, errors, mtl, optparse-applicative, stdenv, text
+      , transformers, turtle
+      }:
+      mkDerivation {
+        pname = "umu-react-basic";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          ansi-terminal base bytestring classy-prelude errors mtl
+          optparse-applicative text transformers turtle
+        ];
+        executableHaskellDepends = [ base classy-prelude ];
+        license = "unknown";
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
